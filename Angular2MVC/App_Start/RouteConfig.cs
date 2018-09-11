178b52054f9f23d8e2b0e79ace939f63ec30c72c
JavDev <jgonzalez717@yahoo.com>
2018-09-10 20:06:22 -0500

    28

--- Angular2MVC/App_Start/RouteConfig.cs
+++ Angular2MVC/App_Start/RouteConfig.cs
@@ -12,12 +12,11 @@ namespace Angular2MVC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                            name: "Default",
+                            url: "{*anything}",
+                            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                        );
         }
     }
 }
